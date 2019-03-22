578a042f552cdf74a1ef51134e4851de6c09d521
lipengzhou <lpzmail@163.com>
2019-03-22 12:00:48 +0800

    Layout组件-使用NavMenu导航菜单组件

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -14,7 +14,75 @@
     </el-row>
   </el-header>
   <el-container>
-    <el-aside width="200px">Aside</el-aside>
+    <el-aside width="200px">
+      <!-- 导航组件 -->
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>用户管理</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="1-1">用户列表</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>权限管理</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="2-1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>角色列表</span>
+              </template>
+            </el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group>
+            <el-menu-item index="2-2">权限列表</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-submenu index="3">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>商品管理</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="3-1">商品列表</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group>
+            <el-menu-item index="3-2">分类参数</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group>
+            <el-menu-item index="3-2">商品分类</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-submenu index="4">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>订单管理</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="4-1">订单列表</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+        <el-submenu index="5">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>数据统计</span>
+          </template>
+          <el-menu-item-group>
+            <el-menu-item index="5-1">数据报表</el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+      </el-menu>
+      <!-- /导航组件 -->
+    </el-aside>
     <el-main>Main</el-main>
   </el-container>
 </el-container>
@@ -25,6 +93,14 @@ export default {
   name: 'Layout',
   data () {
     return {}
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
   }
 }
 </script>
@@ -50,5 +126,9 @@ export default {
 
 .el-main {
   background-color: #e9eef3;
+}
+
+.el-menu {
+  height: 100%;
 }
 </style>
