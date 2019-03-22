4bcae3a0836f1cc4c3a551a598a50c46a3c2b6d1
lipengzhou <lpzmail@163.com>
2019-03-22 10:10:55 +0800

    配置element-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 注册 ElementUI 全局组件
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
