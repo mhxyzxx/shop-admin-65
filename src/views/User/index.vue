60801662e53fede1c3c9ec5cbd46c014adcf6933
lipengzhou <lpzmail@163.com>
2019-03-24 11:29:38 +0800

    用户列表-使用表格组件展示基本内容

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -12,6 +12,41 @@
   </div>
   <!-- /卡片头部 -->
   <!-- 卡片内容 -->
+
+  <!--
+    表格组件
+    data 是表格的数据
+    stripe 斑马纹
+    el-table-column 表格列组件
+      你只需要设计表格列：列名+列值
+      label 列名
+      prop 数据名
+      width 列宽，默认 px
+    它内部会自动去遍历 data 数据，循环生成表格列
+  -->
+  <el-table
+    :data="users"
+    stripe
+    style="width: 100%">
+    <el-table-column
+      type="index">
+    </el-table-column>
+    <el-table-column
+      prop="username"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="email"
+      label="邮箱"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="mobile"
+      label="电话">
+    </el-table-column>
+  </el-table>
+  <!-- /表格组件 -->
 </el-card>
 </template>
 
@@ -21,14 +56,21 @@ import { getUserList } from '@/api/user'
 export default {
   name: 'UserList',
   data () {
-    return {}
+    return {
+      users: []
+    }
   },
   async created () {
     console.log('2. token 存在，进入了具体的业务组件')
     // 除了登录接口，其它接口都需要身份令牌才能访问
     // 所有需要授权（提供 token）的接口都需要像下面这样来写
-    const { data } = await getUserList({ pagesize: 1 })
-    console.log(data)
+    this.loadUsers()
+  },
+  methods: {
+    async loadUsers () {
+      const { data } = await getUserList({ pagenum: 1, pagesize: 100 })
+      this.users = data.users
+    }
   }
 }
 </script>
