915670d8ededb588d54146f7ca40d339931aa379
lipengzhou <lpzmail@163.com>
2019-03-24 12:10:13 +0800

    添加用户-使用dialog对话框组件布局

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -1,71 +1,100 @@
 <template>
-<el-card class="box-card">
-  <!-- 卡片头部 -->
-  <div slot="header" class="clearfix">
-    <!-- 面包屑路径导航组件 -->
-    <el-breadcrumb separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
-      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
-    </el-breadcrumb>
-    <!-- /面包屑路径导航组件 -->
-  </div>
+<div>
+  <el-card class="box-card">
+    <!-- 卡片头部 -->
+    <div slot="header" class="clearfix">
+      <!-- 面包屑路径导航组件 -->
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+        <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+      </el-breadcrumb>
+      <!-- /面包屑路径导航组件 -->
+    </div>
 
-  <!-- 操作选项 -->
-  <el-row :gutter="20">
-    <el-col :span="4">
-      <el-input
-        placeholder="请输入内容"
-        v-model="searchText">
-        <i slot="prefix" class="el-input__icon el-icon-search"></i>
-      </el-input>
-    </el-col>
-    <el-col :span="4">
-      <el-button type="primary">添加用户</el-button>
-    </el-col>
-  </el-row>
-  <!-- /操作选项 -->
+    <!-- 操作选项 -->
+    <el-row :gutter="20">
+      <el-col :span="4">
+        <el-input
+          placeholder="请输入内容"
+          v-model="searchText">
+          <i slot="prefix" class="el-input__icon el-icon-search"></i>
+        </el-input>
+      </el-col>
+      <el-col :span="4">
+        <el-button type="primary">添加用户</el-button>
+      </el-col>
+    </el-row>
+    <!-- /操作选项 -->
 
-  <!-- /卡片头部 -->
-  <!-- 卡片内容 -->
+    <!-- /卡片头部 -->
+    <!-- 卡片内容 -->
+
+    <!--
+      表格组件
+      data 是表格的数据
+      stripe 斑马纹
+      border 带边框
+      el-table-column 表格列组件
+        你只需要设计表格列：列名+列值
+        label 列名
+        prop 数据名
+        width 列宽，默认 px
+      它内部会自动去遍历 data 数据，循环生成表格列
+    -->
+    <el-table
+      :data="users"
+      stripe
+      border
+      style="width: 100%">
+      <el-table-column
+        type="index">
+      </el-table-column>
+      <el-table-column
+        prop="username"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="email"
+        label="邮箱"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="mobile"
+        label="电话">
+      </el-table-column>
+    </el-table>
+    <!-- /表格组件 -->
+  </el-card>
 
   <!--
-    表格组件
-    data 是表格的数据
-    stripe 斑马纹
-    border 带边框
-    el-table-column 表格列组件
-      你只需要设计表格列：列名+列值
-      label 列名
-      prop 数据名
-      width 列宽，默认 px
-    它内部会自动去遍历 data 数据，循环生成表格列
+  添加用户对话框
+  title 对话框标题
+  visible 布尔值，对话框是否显示
   -->
-  <el-table
-    :data="users"
-    stripe
-    border
-    style="width: 100%">
-    <el-table-column
-      type="index">
-    </el-table-column>
-    <el-table-column
-      prop="username"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="email"
-      label="邮箱"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="mobile"
-      label="电话">
-    </el-table-column>
-  </el-table>
-  <!-- /表格组件 -->
-</el-card>
+  <el-dialog title="添加用户" :visible.sync="addFormVisible" width="40%">
+    <el-form :model="addFromData" size="mini" label-position="left">
+      <el-form-item label="用户名" label-width="80px">
+        <el-input v-model="addFromData.username" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" label-width="80px">
+        <el-input v-model="addFromData.password" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="邮箱" label-width="80px">
+        <el-input v-model="addFromData.email" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="电话" label-width="80px">
+        <el-input v-model="addFromData.mobile" autocomplete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="addFormVisible = false">取 消</el-button>
+      <el-button type="primary" @click="addFormVisible = false">确 定</el-button>
+    </div>
+  </el-dialog>
+  <!-- /添加用户对话框 -->
+</div>
 </template>
 
 <script>
@@ -76,7 +105,14 @@ export default {
   data () {
     return {
       users: [],
-      searchText: ''
+      searchText: '',
+      addFormVisible: true, // 对话框是否显示
+      addFromData: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      }
     }
   },
   async created () {
