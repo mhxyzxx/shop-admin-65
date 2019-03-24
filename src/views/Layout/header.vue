ee7d4d4327ad929c856787dd4053f707327653a8
lipengzhou <lpzmail@163.com>
2019-03-24 10:59:35 +0800

    提取Header和NavMenu组件

---
+++ src/views/Layout/header.vue
@@ -0,0 +1,25 @@
+<template>
+<el-row class="app-header">
+  <el-col :span="4">
+    <img src="./logo.png" alt="黑马程序员">
+  </el-col>
+  <el-col :span="16">
+    黑马程序员
+  </el-col>
+  <el-col :span="4">
+    <a href="#">退出</a>
+  </el-col>
+</el-row>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,92 +1,12 @@
 <template>
 <el-container>
   <el-header>
-    <el-row class="app-header">
-      <el-col :span="4">
-        <img src="./logo.png" alt="黑马程序员">
-      </el-col>
-      <el-col :span="16">
-        黑马程序员
-      </el-col>
-      <el-col :span="4">
-        <a href="#">退出</a>
-      </el-col>
-    </el-row>
+    <AppHeader></AppHeader>
   </el-header>
   <el-container>
     <el-aside width="200px">
       <!-- 导航组件 -->
-      <el-menu
-        default-active="2"
-        class="el-menu-vertical-demo"
-        @open="handleOpen"
-        @close="handleClose">
-        <el-submenu index="1">
-          <template slot="title">
-            <!-- <i class="el-icon-location"></i> -->
-            <i class="fas fa-user fa-lg"></i>
-            <span>用户管理</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="1-1">
-              <template slot="title">
-                <i class="el-icon-info"></i>
-                <span>用户列表</span>
-              </template>
-            </el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-submenu index="2">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>权限管理</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="2-1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>角色列表</span>
-              </template>
-            </el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group>
-            <el-menu-item index="2-2">权限列表</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-submenu index="3">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>商品管理</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="3-1">商品列表</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group>
-            <el-menu-item index="3-2">分类参数</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group>
-            <el-menu-item index="3-2">商品分类</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-submenu index="4">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>订单管理</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="4-1">订单列表</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-        <el-submenu index="5">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span>数据统计</span>
-          </template>
-          <el-menu-item-group>
-            <el-menu-item index="5-1">数据报表</el-menu-item>
-          </el-menu-item-group>
-        </el-submenu>
-      </el-menu>
+      <NavMenu></NavMenu>
       <!-- /导航组件 -->
     </el-aside>
     <el-main>
@@ -98,18 +18,19 @@
 </template>
 
 <script>
+import AppHeader from './header.vue'
+import NavMenu from './navmenu.vue'
+
 export default {
   name: 'Layout',
   data () {
     return {}
   },
   methods: {
-    handleOpen (key, keyPath) {
-      console.log(key, keyPath)
-    },
-    handleClose (key, keyPath) {
-      console.log(key, keyPath)
-    }
+  },
+  components: {
+    AppHeader,
+    NavMenu
   }
 }
 </script>
