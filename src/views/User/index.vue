169151043785945b6bffdd089a4f50543fc55e15
lipengzhou <lpzmail@163.com>
2019-03-24 09:23:42 +0800

    通过需要授权的接口校验token的有效性

--- src/views/User/index.vue
+++ src/views/User/index.vue
@@ -5,10 +5,28 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'UserList',
   data () {
     return {}
+  },
+  async created () {
+    // 除了登录接口，其它接口都需要身份令牌才能访问
+    const res = await axios({
+      method: 'GET',
+      url: 'http://localhost:8888/api/private/v1/users?pagenum=1&pagesize=5',
+      headers: { // axios 支持通过 headers 选项添加自定义请求头
+        Authorization: window.localStorage.getItem('token')
+      }
+    })
+    if (res.data.meta.status === 401) {
+      // 401 说明用户身份有问题
+      this.$router.push('/login')
+    } else {
+      console.log(res.data.data)
+    }
   }
 }
 </script>
