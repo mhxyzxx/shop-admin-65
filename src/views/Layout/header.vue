92574a8bbf736c69222e0bd11ff34b17e71e8934
lipengzhou <lpzmail@163.com>
2019-03-25 09:07:28 +0800

    用户退出

--- src/views/Layout/header.vue
+++ src/views/Layout/header.vue
@@ -7,7 +7,7 @@
     黑马程序员
   </el-col>
   <el-col :span="4">
-    <a href="#">退出</a>
+    <el-button type="primary" @click="hadleLogout">退出</el-button>
   </el-col>
 </el-row>
 </template>
@@ -17,6 +17,29 @@ export default {
   name: 'AppHeader',
   data () {
     return {}
+  },
+  methods: {
+    hadleLogout () {
+      this.$confirm('确认退出吗？', '退出提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => { // 确认
+        // 1. 清空登录状态 token
+        window.localStorage.removeItem('token')
+        // 2. 跳转到登录页
+        this.$router.replace('/login')
+        this.$message({
+          type: 'success',
+          message: '退出成功！'
+        })
+      }).catch(() => { // 取消
+        this.$message({
+          type: 'info',
+          message: '已取消退出'
+        })
+      })
+    }
   }
 }
 </script>
