cf0ce0a23bbe50da5cbb6addacada17416dbed5a
lipengzhou <lpzmail@163.com>
2019-03-27 10:26:51 +0800

    权限列表-基本数据展示

--- src/views/Roles/index.vue
+++ src/views/Roles/index.vue
@@ -15,7 +15,7 @@
 
   <!-- 角色列表 -->
   <el-table
-    :data="tableData"
+    :data="roles"
     border
     stripe
     style="width: 100%">
@@ -24,19 +24,16 @@
         hello world
       </template>
     </el-table-column>
+    <el-table-column type="index"></el-table-column>
     <el-table-column
-      prop="date"
-      label="日期"
+      prop="roleName"
+      label="角色名称"
       width="180">
     </el-table-column>
     <el-table-column
-      prop="name"
-      label="姓名"
+      prop="roleDesc"
+      label="描述"
       width="180">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址">
     </el-table-column>
     <el-table-column label="操作">
       <template slot-scope="scope">
@@ -59,30 +56,25 @@
 </template>
 
 <script>
+import { getRoleList } from '@/api/role'
+
 export default {
   name: 'RolesList',
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      roles: []
     }
   },
+  created () {
+    this.loadRoles()
+  },
   methods: {
+    async loadRoles () {
+      const { data, meta } = await getRoleList()
+      if (meta.status === 200) {
+        this.roles = data
+      }
+    },
     handleEdit () {},
     handleDelete () {},
   }
